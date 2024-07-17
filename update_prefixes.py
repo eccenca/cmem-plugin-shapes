823eb1c38d28c8d543d5b69363c1cfb1a293d563
muddymudskipper <thomas.wilmering@gmail.com>
2024-07-17 11:28:03 +0100

    edit

--- update_prefixes.py
+++ update_prefixes.py
@@ -4,8 +4,8 @@ from json import dump, loads
 from pathlib import Path
 from urllib.request import urlopen
 
-with urlopen('http://prefix.cc/popular/all.file.json') as f:
-    json_data = loads(f.read().decode('utf-8'))
+with urlopen("http://prefix.cc/popular/all.file.json") as f:
+    json_data = loads(f.read().decode("utf-8"))
 
 with (Path("cmem_plugin_shapes") / "prefix.cc.json").open("w") as json_file:
     dump(json_data, json_file, sort_keys=True, indent=2)
