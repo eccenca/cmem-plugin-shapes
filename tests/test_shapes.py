fd461e1d67a96b5adc860e3b7e8223bdee5918d2
saipraneeth <2506664+msaipraneeth@users.noreply.github.com>
2025-01-30 07:13:25 +0000

    fix pytest

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -1,84 +1,78 @@
 """Plugin tests."""
 
 from collections.abc import Generator
-from contextlib import suppress
-from json import loads
 from pathlib import Path
-from urllib.request import urlopen
+from shutil import rmtree
+from tempfile import mkdtemp
 
 import pytest
-from cmem.cmempy.dp.proxy.graph import delete, get, post_streamed
-from cmem.cmempy.dp.proxy.sparql import post as post_select
-from cmem.cmempy.dp.proxy.update import post as post_update
-from cmem.cmempy.workspace.projects.project import delete_project, make_new_project
+from cmem.cmempy.dp.proxy.graph import get
 from rdflib import Graph
-from rdflib.compare import isomorphic
+from rdflib.compare import isomorphic, to_isomorphic, graph_diff
 
-from cmem_plugin_shapes.plugin_shapes import PREFIX_CC, ShapesPlugin
+from cmem_plugin_shapes.plugin_shapes import ShapesPlugin
 from tests.utils import TestExecutionContext, needs_cmem
+from .cmemc_command_utils import run_without_assertion, run
 
-from . import __path__
+FIXTURE_DIR = str(Path(__file__).parent / "fixture_dir")
 
 UUID = "5072e1e3e96c40389116a6833d9a3867"
 PROJECT_NAME = f"shapes_plugin_test_{UUID}"
-RESULT_IRI = f"https://eccenca.com/shapes_plugin/{UUID}/shapes/"
-DATA_IRI = f"https://eccenca.com/shapes_plugin/{UUID}/data/"
+
+
+GRAPHS = {
+    "shapes": {
+        "location": f"{FIXTURE_DIR}/test_shapes.ttl",
+        "iri": f"http://docker.localhost/my-persons-shapes",
+    },
+    "dataset": {
+        "location": f"{FIXTURE_DIR}/test_shapes_data.ttl",
+        "iri": f"http://docker.localhost/my-persons",
+    },
+}
 
 
 @pytest.fixture
-def setup() -> Generator:
-    """Create DI project"""
-
-    def remove_import() -> None:
-        query = f"""
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        DELETE DATA {{
-            GRAPH <https://vocab.eccenca.com/shacl/> {{
-                <https://vocab.eccenca.com/shacl/> owl:imports <{RESULT_IRI}>
-            }}
-        }}
-        """
-        post_update(query=query)
-
-    with suppress(Exception):
-        delete_project(PROJECT_NAME)
-    make_new_project(PROJECT_NAME)
-
-    res = post_streamed(DATA_IRI, str(Path(__path__[0]) / "test_shapes_data.ttl"), replace=True)
-    if res.status_code != 204:  # noqa: PLR2004
-        raise ValueError(f"Response {res.status_code}: {res.url}")
+def graph_setup() -> Generator[None, None, None]:
+    """Graph setup fixture"""
+    # make backup and delete all GRAPHS
+    backup_directory = mkdtemp(prefix="cmemc-GRAPHS-backup")
+    for _ in GRAPHS.values():
+        run_without_assertion(["graph", "delete", _["iri"]])
+        run(["graph", "import", _["location"], _["iri"]])
+    run_without_assertion(["project", "delete", PROJECT_NAME])
+    run(["project", "create", PROJECT_NAME])
     yield None
-    delete_project(PROJECT_NAME)
-    remove_import()
-    delete(DATA_IRI)
-    delete(RESULT_IRI)
+    # remove test GRAPHS
+    for _ in GRAPHS.values():
+        run(["graph", "delete", _["iri"]])
+
+    # import backup GRAPHS and compare triple counts
+    run(["graph", "import", backup_directory])
+    rmtree(backup_directory)
 
 
 @needs_cmem
-@pytest.mark.usefixtures("setup")
+@pytest.mark.usefixtures("graph_setup")
 def test_workflow_execution() -> None:
     """Test plugin execution"""
+    data_graph_iri = GRAPHS["dataset"]["iri"]
+    shapes_graph_iri = GRAPHS["shapes"]["iri"]
     ShapesPlugin(
-        data_graph_iri=DATA_IRI,
-        shapes_graph_iri=RESULT_IRI,
+        data_graph_iri=data_graph_iri,
+        shapes_graph_iri=shapes_graph_iri,
         overwrite=True,
-        import_shapes=True,
+        import_shapes=False,
         prefix_cc=False,
     ).execute(inputs=[], context=TestExecutionContext(project_id=PROJECT_NAME))
-
-    query = f"""
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    SELECT ?o {{
-        GRAPH <https://vocab.eccenca.com/shacl/> {{
-            <https://vocab.eccenca.com/shacl/> owl:imports ?o
-        }}
-        FILTER( ?o = <{RESULT_IRI}> )
-    }}
-    """
-    result_import = loads(post_select(query=query))
-    assert len(result_import["results"]["bindings"]) == 1
-
-    result_graph = Graph().parse(data=get(RESULT_IRI, owl_imports_resolution=False).text)
-    # result_graph.serialize("tests/result_graph.ttl", format="turtle")  # noqa: ERA001
-    test = Graph().parse(Path(__path__[0]) / "test_shapes.ttl", format="turtle")
+    result_graph = Graph().parse(data=get(shapes_graph_iri, owl_imports_resolution=False).text)
+    test = Graph().parse(f"{FIXTURE_DIR}/test_shapes.ttl")
     assert isomorphic(result_graph, test)
+    with pytest.raises(ValueError, match="Graph <http://docker.localhost/my-persons-shapes> already exists."):
+        ShapesPlugin(
+            data_graph_iri=data_graph_iri,
+            shapes_graph_iri=shapes_graph_iri,
+            overwrite=False,
+            import_shapes=False,
+            prefix_cc=False,
+        ).execute(inputs=[], context=TestExecutionContext(project_id=PROJECT_NAME))
