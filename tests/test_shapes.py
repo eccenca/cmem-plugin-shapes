0223cfb653a720c9bad2b6f88d00a73efc799e74
saipraneeth <2506664+msaipraneeth@users.noreply.github.com>
2025-01-31 06:05:35 +0000

    add test case for import graph

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -1,5 +1,6 @@
 """Plugin tests."""
 
+import json
 from collections.abc import Generator
 from pathlib import Path
 from shutil import rmtree
@@ -7,8 +8,9 @@ from tempfile import mkdtemp
 
 import pytest
 from cmem.cmempy.dp.proxy.graph import get
+from cmem.cmempy.dp.proxy.sparql import get as ask
 from rdflib import Graph
-from rdflib.compare import graph_diff, isomorphic, to_isomorphic
+from rdflib.compare import isomorphic
 
 from cmem_plugin_shapes.plugin_shapes import ShapesPlugin
 from tests.utils import TestExecutionContext, needs_cmem
@@ -32,15 +34,28 @@ GRAPHS = {
     },
 }
 
+ECCENCA_SHAPES_CATELOG = {
+    "location": f"{FIXTURE_DIR}/test_shapes_eccenca.ttl",
+    "iri": "https://vocab.eccenca.com/shacl/",
+}
+
+ASK_QUERY = """PREFIX owl: <http://www.w3.org/2002/07/owl#>
+ASK
+{
+  GRAPH <https://vocab.eccenca.com/shacl/> {
+    <https://vocab.eccenca.com/shacl/> owl:imports <http://docker.localhost/my-persons-shapes>
+  }
+}"""
+
 
 @pytest.fixture
 def graph_setup() -> Generator[None, None, None]:
     """Graph setup fixture"""
     # make backup and delete all GRAPHS
     backup_directory = mkdtemp(prefix="cmemc-GRAPHS-backup")
-    for _ in GRAPHS.values():
-        run_without_assertion(["graph", "delete", _["iri"]])
-        run(["graph", "import", _["location"], _["iri"]])
+    run(["graph", "export", "--all", "--output-dir", backup_directory])
+    run(["graph", "delete", "--all"])
+    run(["graph", "import", GRAPHS["dataset"]["location"], GRAPHS["dataset"]["iri"]])
     run_without_assertion(["project", "delete", PROJECT_NAME])
     run(["project", "create", PROJECT_NAME])
     yield None
@@ -79,3 +94,27 @@ def test_workflow_execution() -> None:
             import_shapes=False,
             prefix_cc=False,
         ).execute(inputs=[], context=TestExecutionContext(project_id=PROJECT_NAME))
+
+
+@needs_cmem
+@pytest.mark.usefixtures("graph_setup")
+def test_import_shapes() -> None:
+    """Test plugin execution with import shapes"""
+    data_graph_iri = GRAPHS["dataset"]["iri"]
+    shapes_graph_iri = GRAPHS["shapes"]["iri"]
+    ShapesPlugin(
+        data_graph_iri=data_graph_iri,
+        shapes_graph_iri=shapes_graph_iri,
+        overwrite=True,
+        import_shapes=False,
+        prefix_cc=False,
+    ).execute(inputs=[], context=TestExecutionContext(project_id=PROJECT_NAME))
+    assert not json.loads(ask(query=ASK_QUERY)).get("boolean", True)
+    ShapesPlugin(
+        data_graph_iri=data_graph_iri,
+        shapes_graph_iri=shapes_graph_iri,
+        overwrite=True,
+        import_shapes=True,
+        prefix_cc=False,
+    ).execute(inputs=[], context=TestExecutionContext(project_id=PROJECT_NAME))
+    assert json.loads(ask(query=ASK_QUERY)).get("boolean", False)
